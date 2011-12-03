48f029b32e71c9aa50d2db6251a1bf6812b975d2
Kevin Boos <kevinaboos@gmail.com>
2011-12-03 14:49:06 -0600

    added #defines for modes

--- topShared.h
+++ topShared.h
@@ -1,6 +1,9 @@
 #ifndef _TOP_SHARED_H_
 #define _TOP_SHARED_H_
 
+#define	MODE_ECB_ENC	1
+#define MODE_ECB_DEC	2
+
 extern unsigned char input_block[16];
 extern unsigned char IV_block[16];
 extern unsigned char input_key[16];
