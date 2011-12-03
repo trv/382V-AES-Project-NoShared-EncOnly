b9cf06291838d271e1b192a95a4be31b87aa5c66
T R Van Ruitenbeek <tvanruitenbeek@yoshi.ece.utexas.edu>
2011-12-03 14:16:46 -0600

    added topShared.h

---
+++ topShared.h
@@ -0,0 +1,10 @@
+#ifndef _TOP_SHARED_H_
+#define _TOP_SHARED_H_
+
+extern unsigned char input_block[16];
+extern unsigned char IV_block[16];
+extern unsigned char input_key[16];
+extern unsigned char output_block[16];
+
+#endif
+
